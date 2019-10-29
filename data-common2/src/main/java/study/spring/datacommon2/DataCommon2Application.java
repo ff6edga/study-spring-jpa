df3e317304a74d5cc101ec1791771fcb6c637de5
김윤수 <younsookim@tmon.co.kr>
2019-10-29 19:00:31 +0900

    created new example

---
+++ data-common2/src/main/java/study/spring/datacommon2/DataCommon2Application.java
@@ -0,0 +1,13 @@
+package study.spring.datacommon2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCommon2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCommon2Application.class, args);
+	}
+
+}
