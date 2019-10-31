76930d63a68b1e557af6470e94059f0ba38e65bf
김윤수 <younsookim@tmon.co.kr>
2019-10-31 18:36:52 +0900

    스프링 데이터 Common 12. 웹 기능 2부 DomainClassConverter

---
+++ common-web/src/test/java/study/spring/commonweb/CommonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.commonweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ common-web/src/test/java/study/spring/commonweb/post/PostControllerTest.java
@@ -0,0 +1,44 @@
+package study.spring.commonweb.post;
+
+import javafx.geometry.Pos;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+/* 아래 어노테이션과 함께 /test/resources/application-test.properties
+ * 에 테스트시 사용할 내용만 덧붙여서 사용할 수 있다.
+ */
+//@ActiveProfiles("test")
+public class PostControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	PostRepository postRepository;
+
+	@Test
+	public void getPost() throws Exception {
+		Post post = new Post();
+		post.setTitle("jpa");
+		postRepository.save(post);
+
+		mockMvc.perform(get("/posts/" + post.getId()))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string("jpa"));
+	}
+}
