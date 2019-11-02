eef506ee75f1544c729363039f9177dd8b3e7775
김윤수 <younsookim@tmon.co.kr>
2019-11-02 17:03:24 +0900

    스프링 데이터 Common 13. 웹 기능 3부 Pageable과 Sort

--- common-web/src/test/java/study/spring/commonweb/post/PostControllerTest.java
+++ common-web/src/test/java/study/spring/commonweb/post/PostControllerTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -51,10 +54,12 @@ public class PostControllerTest {
 		mockMvc.perform(get("/posts/")
 					.param("page", "0")
 					.param("size", "10")
-					.param("sort", "created.desc")
+					.param("sort", "created,desc")
 					.param("sort", "title"))
 				.andDo(print())
-				.andExpect(status().isOk());
+				.andExpect(status().isOk())
+				// 강의상의 코드와 약간 다르다.
+				.andExpect(jsonPath("content[0].title").value("jpa"));
 	}
 
 
