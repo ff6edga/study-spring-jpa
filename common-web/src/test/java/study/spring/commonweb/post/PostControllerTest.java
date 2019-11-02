b4e7a29f5efb3f1818dab46539b622d0fb41eeb8
김윤수 <younsookim@tmon.co.kr>
2019-11-02 19:16:39 +0900

    스프링 데이터 Common 14. 웹 기능 4부 HATEOAS

--- common-web/src/test/java/study/spring/commonweb/post/PostControllerTest.java
+++ common-web/src/test/java/study/spring/commonweb/post/PostControllerTest.java
@@ -1,22 +1,16 @@
 package study.spring.commonweb.post;
 
-import javafx.geometry.Pos;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -47,12 +41,10 @@ public class PostControllerTest {
 
 	@Test
 	public void getPosts() throws Exception {
-		Post post = new Post();
-		post.setTitle("jpa");
-		postRepository.save(post);
+		createPosts();
 
 		mockMvc.perform(get("/posts/")
-					.param("page", "0")
+					.param("page", "3")
 					.param("size", "10")
 					.param("sort", "created,desc")
 					.param("sort", "title"))
@@ -62,5 +54,14 @@ public class PostControllerTest {
 				.andExpect(jsonPath("content[0].title").value("jpa"));
 	}
 
+	private void createPosts() {
+		int postCount = 100;
+		while(postCount > 0) {
+			Post post = new Post();
+			post.setTitle("jpa");
+			postRepository.save(post);
+			postCount--;
+		}
+	}
 
 }
