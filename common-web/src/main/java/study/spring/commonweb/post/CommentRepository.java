dc511daef392b632bc014c35c60a5aa7f79505f8
김윤수 <younsookim@tmon.co.kr>
2019-11-18 17:33:04 +0900

    스프링 데이터 JPA 11.트랜잭션

--- common-web/src/main/java/study/spring/commonweb/post/CommentRepository.java
+++ common-web/src/main/java/study/spring/commonweb/post/CommentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +16,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpec
 	Optional<Comment> getById(Long id);
 
 	//List<CommentSummary> findByPost_Id(Long id);
+	@Transactional(readOnly = true)
 	<T> List<T> findByPost_Id(Long id, Class<T> type);
 }
